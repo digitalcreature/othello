2db9eec5048741a5ffd12cb2c9629f886901d52b
Tanner Grehawick <tgrehawi@gmail.com>
2017-02-22 18:40:35 -0500

    api: vote submission

--- othello/views.py
+++ othello/views.py
@@ -1,6 +1,8 @@
 from . import *
 
 TEAMS = ["white", "black"] # valid teams
+BOARD_ROW_COUNT = 8
+BOARD_COL_COUNT = 8
 
 def json_response(j, code=200):
 	'''json formatted response'''
@@ -25,3 +27,13 @@ def choose_team(team):
 			"error": "already on team",
 			"team": session["team"]
 		}, 400)
+
+@app.route("/vote", methods=["POST"])
+def vote():
+	'''vote for a square'''
+	row = int(request.args["r"])
+	col = int(request.args["c"])
+	if row >= 0 and row < BOARD_ROW_COUNT and col >= 0 and col < BOARD_COL_COUNT:
+		return json_response({"row": row, "col": col})
+	else:
+		return json_response({"error": "vote square out of bounds"}, 400)
